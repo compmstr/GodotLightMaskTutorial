f3938c86e1f0f6037774cd9e2b4e7873e6ac123d
Corey Williams <corey@undimk5>
2022-10-06 08:23:12 -0400

    Work on calculating tilemap-based UV coordinates based on screen UV
    Not working quite yet, but I did learn that the tilemap UV coordinates in
    shaders are per-tile, and not per-tilemap

--- Scripts/light_mask_tutorial.gd
+++ Scripts/light_mask_tutorial.gd
@@ -12,6 +12,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	camera.position = player.position
+	var tilemap_shader = tile_map.material as ShaderMaterial
+	tilemap_shader.set_shader_parameter("screen_start", camera.get_canvas_transform().get_origin())
+	tilemap_shader.set_shader_parameter("screen_size", get_viewport().size / 1)
+	tilemap_shader.set_shader_parameter("tilemap_size", tile_map.get_used_rect().size * tile_map.tile_set.tile_size)
+	var tilemap_rect = tile_map.get_viewport_rect()
+	tilemap_rect.position += camera.get_canvas_transform().get_origin()
+	# print("Tilemap rect: {0}".format([tilemap_rect]))
+	# print("Screen start: {0}".format([camera.get_canvas_transform().get_origin()]))
+	# print("Tilemap size: {0}".format([tile_map.get_used_rect().size * tile_map.tile_set.tile_size]))
+	tilemap_shader.set_shader_parameter("tilemap_start", tile_map.position)
 
 func _input(event):
 	if event is InputEventKey:
