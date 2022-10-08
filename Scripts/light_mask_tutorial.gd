e58d9e08b14935907330c29b7783140dedf869d8
Corey Williams <corey@undimk5>
2022-10-07 20:09:15 -0400

    Got the tilemap-uv shader working finally
    Set up signal connection to keep visibility viewport the correct size

--- Scripts/light_mask_tutorial.gd
+++ Scripts/light_mask_tutorial.gd
@@ -1,27 +1,38 @@
 extends Node2D
 
-@onready var player: CharacterBody2D = $Player
+@onready var player: CharacterBody2D = $LitView/Player
 @onready var camera: Camera2D = $Camera2d
 @onready var tile_map: TileMap = $TileMap
 
+@onready var masked_view: TileMap = $MaskedView
+
+@onready var vis_view: SubViewport = $VisibilityViewport
+
+func _on_viewport_size_change():
+	var new_size = get_viewport().size
+	vis_view.size = new_size
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_viewport().connect("size_changed", self._on_viewport_size_change)
 	pass # Replace with function body.
 
+func update_mask_shader():
+	var tm_screen_start = get_viewport_transform() * (tile_map.get_global_transform() * Vector2(tile_map.get_used_rect().position))
+	var tm_screen_end = get_viewport_transform() * (tile_map.get_global_transform() * Vector2(tile_map.get_used_rect().end * tile_map.tile_set.tile_size))
+	var screen_rect = get_viewport_transform() * get_viewport_rect()
+	var sm = masked_view.material as ShaderMaterial
+	sm.set_shader_parameter("screen_start", screen_rect.position)
+	sm.set_shader_parameter("screen_size", screen_rect.size)
+	sm.set_shader_parameter("tilemap_start", tm_screen_start)
+	sm.set_shader_parameter("mask_size", get_viewport_transform().get_scale() * Vector2(vis_view.size))
+	# print("ss: {0} & {1}\ntm: {2} & {3}".format([screen_rect.position, screen_rect.size, tm_screen_start, tm_screen_end - tm_screen_start]))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	camera.position = player.position
-	var tilemap_shader = tile_map.material as ShaderMaterial
-	tilemap_shader.set_shader_parameter("screen_start", camera.get_canvas_transform().get_origin())
-	tilemap_shader.set_shader_parameter("screen_size", get_viewport().size / 1)
-	tilemap_shader.set_shader_parameter("tilemap_size", tile_map.get_used_rect().size * tile_map.tile_set.tile_size)
-	var tilemap_rect = tile_map.get_viewport_rect()
-	tilemap_rect.position += camera.get_canvas_transform().get_origin()
-	# print("Tilemap rect: {0}".format([tilemap_rect]))
-	# print("Screen start: {0}".format([camera.get_canvas_transform().get_origin()]))
-	# print("Tilemap size: {0}".format([tile_map.get_used_rect().size * tile_map.tile_set.tile_size]))
-	tilemap_shader.set_shader_parameter("tilemap_start", tile_map.position)
+	update_mask_shader()
+
 
 func _input(event):
 	if event is InputEventKey:
